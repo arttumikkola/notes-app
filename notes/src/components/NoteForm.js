5c5c81dcc57e717fdb413f98afee58adfd705da1
Arttu Mikkola <arttu.mikkola@windowslive.com>
2022-12-16 16:03:53 +0200

    Update addNote function and add possibility to add tags

--- notes/src/components/NoteForm.js
+++ notes/src/components/NoteForm.js
@@ -3,24 +3,51 @@ import Button from "rsuite/Button";
 import "../App.css";
 import "rsuite/dist/rsuite.min.css";
 
-const NoteForm = ({ setNotes }) => {
+const NoteForm = ({ notes, setNotes }) => {
   const [noteInput, setNoteInput] = useState("");
+  const [tagInput, setTagInput] = useState("");
+
+  const getDateAndTime = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // January is 0
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+  };
 
   const addNote = (e) => {
     e.preventDefault();
-    setNotes(noteInput);
+    const noteObject = {
+      content: noteInput,
+      tag: tagInput,
+      date: getDateAndTime(),
+    };
+    setNotes(notes.concat(noteObject));
     setNoteInput("");
+    setTagInput("");
+    console.log(notes);
   };
 
   return (
     <div>
       <form onSubmit={addNote} className="form">
         <input
-          className="input"
+          className="noteInput"
           type="text"
           placeholder="Add note"
           value={noteInput}
           onChange={(e) => setNoteInput(e.target.value)}
+        ></input>
+        <input
+          className="tagInput"
+          type="text"
+          placeholder="Add tag"
+          value={tagInput}
+          onChange={(e) => setTagInput(e.target.value)}
         ></input>
       </form>
       <div className="btnDiv">
@@ -30,6 +57,7 @@ const NoteForm = ({ setNotes }) => {
           appearance="primary"
           color="blue"
           size="sm"
+          onClick={addNote}
         >
           Add note
         </Button>
